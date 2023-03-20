43c2e0485cebb5e5d384bbfca986d0fdf628f79b
MaksimS <sipatovGT@mail.ru>
2023-03-20 12:14:14 +0400

    добавил методы добавления нового контакта и удаления контакта

--- HW5/Menu.java
+++ HW5/Menu.java
@@ -2,6 +2,15 @@ package HW5;
 
 public class Menu {
 
-    System.out.println
+    public void MenuPhonebook() {
+    System.out.println("<Телефонный справочник> \n");
+    System.out.println("Напишите соответствующую цифру, для выбора пункта меню: \n" +  
+                                        "0 - Выход \n" + 
+                                        "1 - Показать все контакты \n" + 
+                                        "2 - Добавить контакт \n" + 
+                                        "3 - Поиск контакта \n" +
+                                        "4 - Удалить контакт \n" +
+                                        "5 - Записать всё");
 
+    }
 }
--- HW5/PhoneBook.java
+++ HW5/PhoneBook.java
@@ -1,5 +1,36 @@
 package HW5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PhoneBook {
     
+    public void addAContact(ArrayList ListContacts) {
+
+        System.out.println("Хотите добавить новый контакт? \n" + 
+                                "enter - да; 0 - нет");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            } else {
+                System.out.println("Введите номер: ");
+                String inputNumber = scanner.nextLine();
+                System.out.println("Введите Имя: ");
+                String Name = scanner.nextLine();
+                System.out.println("Введите фамилию: ");
+                String Surname = scanner.nextLine();
+                System.out.println("Хотите добавить еще один контакт? \n" + 
+                                "enter - да; 0 - нет");
+            }
+        }
+    }
+
+    public void deleteContact(ArrayList ListContacts){
+        System.out.println("Введите порядкой номер для удаления контакта");
+        Scanner scan = new Scanner(System.in);
+        int in = scan.nextInt();
+        ListContacts.remove(in - 1);
+    }
 }
